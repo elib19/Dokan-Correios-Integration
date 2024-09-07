fd16e9ebaf94720e21d31cd810f9b09a16b7daa1
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-07 18:13:08 -0300

    Update uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -1,15 +1,16 @@
 <?php
+
 // Evita acesso direto ao arquivo
-if (!defined('WP_UNINSTALL_PLUGIN')) {
+if (!defined('ABSPATH') || !defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
-// Remove as opções do banco de dados
+// Remover opções do plugin do banco de dados
 delete_option('dci_plugin_installed');
 delete_option('dci_plugin_version');
 delete_option('dci_custom_shipping_log');
 
-// Exclui a tabela de logs de frete, se necessário
+// Opcional: Remover tabelas criadas no banco de dados
 global $wpdb;
 $table_name = $wpdb->prefix . 'dci_shipping_log';
 $wpdb->query("DROP TABLE IF EXISTS $table_name");
