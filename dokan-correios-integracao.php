1f2bcaef64ed8780f0b746a5ecf3820f2961afaa
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-07 18:01:45 -0300

    Update dokan-correios-integracao.php

--- dokan-correios-integracao.php
+++ dokan-correios-integracao.php
@@ -2,21 +2,17 @@
 /*
 Plugin Name: Integração Dokan Correios
 Description: Integra o Dokan Pro com o WooCommerce Correios, utilizando o CEP do vendedor como origem.
-Version: 1.0
+Version: 1.1
 Author: Eli Silva
-Author URI: brasilnarede.online
+Author URI: https://brasilnarede.online
 */
 
-// Evita acesso direto ao arquivo
 if (!defined('ABSPATH')) {
-    exit;
+    exit; // Evita acesso direto
 }
 
-// Inclui os arquivos necessários
+// Inclui arquivos necessários
 require_once plugin_dir_path(__FILE__) . 'includes/activation.php';
 require_once plugin_dir_path(__FILE__) . 'includes/dokan-correios.php';
 require_once plugin_dir_path(__FILE__) . 'includes/helper.php';
-
-// Funções de ativação e desativação
-register_activation_hook(__FILE__, 'dci_activate_plugin');
-register_deactivation_hook(__FILE__, 'dci_deactivate_plugin');
+require_once plugin_dir_path(__FILE__) . 'includes/uninstall.php';
