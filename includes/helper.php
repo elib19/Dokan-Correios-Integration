b5e4fe93757161171474a043b5830dfbc8c0caa7
elib19 <143918479+elib19@users.noreply.github.com>
2024-09-07 14:36:33 -0300

    Update helper.php

--- includes/helper.php
+++ includes/helper.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Funções auxiliares para o plugin de integração Dokan Correios
- */
 
 /**
  * Obtém o CEP do vendedor com base no ID do vendedor.
@@ -9,7 +6,7 @@
  * @param int $seller_id O ID do vendedor.
  * @return string|null O CEP do vendedor, ou null se não encontrado.
  */
-function get_seller_cep( $seller_id ) {
-    $address = get_user_meta( $seller_id, 'dokan_profile_settings', true );
+function get_seller_cep($seller_id) {
+    $address = get_user_meta($seller_id, 'dokan_profile_settings', true);
     return $address['address']['zip'] ?? null;
 }
